a92ff3f7e9e7012d6019043218190417e30d8357
Sahith Bodla <sahith.bodla@gmail.com>
2022-10-26 17:30:29 +0530

    removed hard coding

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -9,12 +9,16 @@ export const unpkgPathPlugin = () => {
         console.log('onResolve', args);
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
-        } else if (args.path === 'tiny-test-pkg') {
-          return {
-            path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
-            namespace: 'a',
-          };
         }
+
+        return { path: `https://unpkg.com/${args.path}`, namespace: 'a' };
+
+        // else if (args.path === 'tiny-test-pkg') {
+        //   return {
+        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+        //     namespace: 'a',
+        //   };
+        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -24,7 +28,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('tiny-test-pkg');
+              const message = require('medium-test-pkg');
               console.log(message);
             `,
           };
