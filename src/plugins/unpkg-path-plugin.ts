09224f2a7f3a807f4ef7eb9ba339c9a2d9abd4bf
Sahith Bodla <sahith.bodla@gmail.com>
2022-10-26 21:14:04 +0530

    fixed bug for nested paths

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -12,7 +12,10 @@ export const unpkgPathPlugin = () => {
         }
         if (args.path.includes('./') || args.path.includes('../')) {
           return {
-            path: new URL(args.path, args.importer + '/').href,
+            path: new URL(
+              args.path,
+              'https://unpkg.com' + args.resolveDir + '/'
+            ).href,
             namespace: 'a',
           };
         }
@@ -26,16 +29,18 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('medium-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           };
         }
 
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
+        console.log('request', request);
         return {
           loader: 'jsx',
           contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
         };
       });
     },
