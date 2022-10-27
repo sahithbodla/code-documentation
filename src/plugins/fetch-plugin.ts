1f4da8512a87498000a6b402fa4574191302a0d4
Sahith Bodla <sahith.bodla@gmail.com>
2022-10-27 21:38:30 +0530

    extracted common caching logic

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -17,7 +17,8 @@ export const fetchPlugin = (inputCode: string) => {
         };
       });
 
-      build.onLoad({ filter: /.css$/ }, async (args) => {
+      // if we don't get a return here, code will not break but continuos execution to next onLoad ╰(*°▽°*)╯
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         // check to see if we have already fetched this file and if it is in cache
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
@@ -27,7 +28,9 @@ export const fetchPlugin = (inputCode: string) => {
         if (cachedResult) {
           return cachedResult;
         }
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args) => {
         const { data, request } = await axios.get(args.path);
         const escaped = data
           .replace(/\n/g, '')
@@ -52,16 +55,6 @@ export const fetchPlugin = (inputCode: string) => {
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        // check to see if we have already fetched this file and if it is in cache
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
-
-        // if it is return immediately
-        if (cachedResult) {
-          return cachedResult;
-        }
-
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
