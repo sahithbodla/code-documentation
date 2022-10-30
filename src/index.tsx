f2fa2b04e166fbd1e5f21ec085e82bc55a4e239d
Sahith Bodla <sahith.bodla@gmail.com>
2022-10-30 18:17:26 +0530

    feat(editor): refactored the code to add multiple code editors

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,12 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import CodeEditor from './components/code-editor';
-import Preview from './components/preview';
-import bundle from './bundler';
-import * as constants from './constants';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [input, setInput] = useState('input');
-  const [code, setCode] = useState('');
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue={constants.initialValue}
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 };
