eaf04d732ea3d5944a09969ada6fb46ecdd3df82
Sahith Bodla <sahith.bodla@gmail.com>
2022-10-30 17:50:06 +0530

    refactor(preview): code refactoring for preview component

---
+++ src/components/preview.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useRef } from 'react';
+
+interface PreviewProps {
+  code: string;
+}
+
+const html = `
+    <html>
+      <head></head>
+      <body>
+        <!-- This is for rendering users react application -->
+        <div id="root"></div>
+        <script>
+          window.addEventListener(
+            'message',
+            (event) => {
+              try { 
+                eval(event.data);
+               } catch(err) {
+                const root = document.querySelector('#root');
+                root.innerHTML = '<div style="color: red;"> <h4>Runtime Error:</h4>' + err + '</div>';
+                console.error(err);
+               }
+              
+            },
+            false
+          );
+        </script>
+      </body>
+    </html>
+  `;
+
+const Preview: React.FC<PreviewProps> = ({ code }) => {
+  const iframe = useRef<any>();
+  useEffect(() => {
+    // Is users accidentally deletes the div of id root to render react application, below
+    // code ensure to reload the iframe contents completely for the next execution
+    iframe.current.srcdoc = html;
+    iframe.current.contentWindow.postMessage(code, '*');
+  }, [code]);
+  return (
+    <iframe
+      title="preview"
+      ref={iframe}
+      srcDoc={html}
+      sandbox="allow-scripts allow-modals"
+    />
+  );
+};
+
+export default Preview;
--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,12 @@ import ReactDOM from 'react-dom/client';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
 
 const App = () => {
   const ref = useRef<any>();
-  const iframe = useRef<any>();
   const [input, setInput] = useState('input');
+  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -26,9 +27,6 @@ const App = () => {
     if (!ref.current) {
       return;
     }
-    // Is users accidentally deletes the div of id root to render react application, below
-    // code ensure to reload the iframe contents completely for the next execution
-    iframe.current.srcdoc = html;
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -40,36 +38,8 @@ const App = () => {
         global: 'window',
       },
     });
-
-    // setCode(result.outputFiles[0].text);
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    setCode(result.outputFiles[0].text);
   };
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <!-- This is for rendering users react application -->
-        <div id="root"></div>
-        <script>
-          window.addEventListener(
-            'message',
-            (event) => {
-              try { 
-                eval(event.data);
-               } catch(err) {
-                const root = document.querySelector('#root');
-                root.innerHTML = '<div style="color: red;"> <h4>Runtime Error:</h4>' + err + '</div>';
-                console.error(err);
-               }
-              
-            },
-            false
-          );
-        </script>
-      </body>
-    </html>
-  `;
 
   const initialValue = `// please type in some JavaScript to see the magic
 const a = 1;`;
@@ -80,19 +50,10 @@ const a = 1;`;
         initialValue={initialValue}
         onChange={(value) => setInput(value)}
       />
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe
-        title="preview"
-        ref={iframe}
-        srcDoc={html}
-        sandbox="allow-scripts allow-modals"
-      />
+      <Preview code={code} />
     </div>
   );
 };
