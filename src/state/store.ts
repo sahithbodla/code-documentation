37fb7666727a013b1906b3b16a0816fd6faa6801
Sahith Bodla <sahith.bodla@gmail.com>
2022-11-16 23:32:40 +0530

    setting up redux store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,1 +1,5 @@
-export {};
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
+export const store = createStore(reducers, {}, applyMiddleware(thunk));
