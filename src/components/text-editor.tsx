a28fcd2254285111e31496e5454ce3b20dbd2963
Sahith Bodla <sahith.bodla@gmail.com>
2022-11-16 22:34:19 +0530

    feat(markdown): added dark theme

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -25,13 +25,13 @@ const TextEditor: React.FC = () => {
   }, []);
   if (editing) {
     return (
-      <div ref={ref}>
+      <div className="text-editor" ref={ref}>
         <MDEditor />
       </div>
     );
   }
   return (
-    <div onClick={() => setEditing(true)}>
+    <div className="text-editor" onClick={() => setEditing(true)}>
       <MDEditor.Markdown source={'# Header'} />
     </div>
   );
