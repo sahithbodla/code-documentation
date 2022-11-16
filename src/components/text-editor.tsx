e4a41c0cc046371fc3af400b2b072acdd22ea45c
Sahith Bodla <sahith.bodla@gmail.com>
2022-11-16 22:16:21 +0530

    feat(markdown): state setup for editing or reading

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,8 +1,36 @@
+import { useState, useEffect, useRef } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        console.log('element clicked on is inside editor');
+        return;
+      }
+      console.log('element clicked is not inside');
+      setEditing(false);
+    };
+    document.addEventListener('click', listener, { capture: true });
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor />
+      </div>
+    );
+  }
   return (
-    <div>
+    <div onClick={() => setEditing(true)}>
       <MDEditor.Markdown source={'# Header'} />
     </div>
   );
