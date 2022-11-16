b4a14695f144f22d73a29c5123faff783474b7e1
Sahith Bodla <sahith.bodla@gmail.com>
2022-11-16 22:21:46 +0530

    fir(markdown): borken cursor due to overlap of css classes with bulma

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import MDEditor from '@uiw/react-md-editor';
+import './text-editor.css';
 
 const TextEditor: React.FC = () => {
   const [editing, setEditing] = useState(false);
