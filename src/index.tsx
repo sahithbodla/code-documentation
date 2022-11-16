95b13445d51442cdf4ae6a4a88856adb70dc49a3
Sahith Bodla <sahith.bodla@gmail.com>
2022-11-16 23:36:03 +0530

    connect to react

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './state';
 // import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
