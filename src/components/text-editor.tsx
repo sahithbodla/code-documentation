2868a09a52e39a624043802aabadcb7bb34650fc
Sahith Bodla <sahith.bodla@gmail.com>
2022-12-01 22:55:46 +0530

    refactor(text-editor): moved value state to redux store

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,11 +1,17 @@
 import { useState, useEffect, useRef } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 import './text-editor.css';
+import { Cell } from '../state';
+import { useActions } from '../hooks';
 
-const TextEditor: React.FC = () => {
+interface TextEditorProps {
+  cell: Cell;
+}
+
+const TextEditor: React.FC<TextEditorProps> = ({ cell }) => {
   const [editing, setEditing] = useState(false);
-  const [value, setValue] = useState('# Header');
   const ref = useRef<HTMLDivElement | null>(null);
+  const { updateCell } = useActions();
   useEffect(() => {
     const listener = (event: MouseEvent) => {
       if (
@@ -27,14 +33,17 @@ const TextEditor: React.FC = () => {
   if (editing) {
     return (
       <div className="text-editor" ref={ref}>
-        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
+        <MDEditor
+          value={cell.content}
+          onChange={(v) => updateCell(cell.id, v || '')}
+        />
       </div>
     );
   }
   return (
     <div className="text-editor card" onClick={() => setEditing(true)}>
       <div className="card-content">
-        <MDEditor.Markdown source={value} />
+        <MDEditor.Markdown source={cell.content || 'Click to edit'} />
       </div>
     </div>
   );
