d0f63fc12cceadf3cec2ed7111ab32424fff2770
Sahith Bodla <sahith.bodla@gmail.com>
2022-12-02 00:20:26 +0530

    style(addcell): added icons and space in between buttons

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -9,9 +9,28 @@ interface AddCellProps {
 const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
   const { insertCellBefore } = useActions();
   return (
-    <div>
-      <button onClick={() => insertCellBefore(nextCellId, 'code')}>Code</button>
-      <button onClick={() => insertCellBefore(nextCellId, 'text')}>Text</button>
+    <div className="add-cell">
+      <div className="add-buttons">
+        <button
+          className="button is-rounded is-primary is-small"
+          onClick={() => insertCellBefore(nextCellId, 'code')}
+        >
+          <span className="icon is-small">
+            <i className="fas fa-plus"></i>
+          </span>
+          <span>Code</span>
+        </button>
+        <button
+          className="button is-rounded is-primary is-small"
+          onClick={() => insertCellBefore(nextCellId, 'text')}
+        >
+          <span className="icon is-small">
+            <i className="fas fa-plus"></i>
+          </span>
+          <span>Text</span>
+        </button>
+      </div>
+      <div className="divider"></div>
     </div>
   );
 };
