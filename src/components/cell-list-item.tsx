91b9b49379340d73db69980395814001368eff19
Sahith Bodla <sahith.bodla@gmail.com>
2022-12-01 23:45:33 +0530

    style(action-bar): added little distinction for code cell

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -11,16 +11,23 @@ interface CellListItemProps {
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
   if (cell.type === 'code') {
-    child = <CodeCell cell={cell} />;
+    child = (
+      <>
+        <div className="action-bar-wrapper">
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
+    );
   } else {
-    child = <TextEditor cell={cell} />;
+    child = (
+      <>
+        <TextEditor cell={cell} />
+        <ActionBar id={cell.id} />
+      </>
+    );
   }
-  return (
-    <div className="cell-list-item">
-      {child}
-      <ActionBar id={cell.id} />
-    </div>
-  );
+  return <div className="cell-list-item">{child}</div>;
 };
 
 export default CellListItem;
