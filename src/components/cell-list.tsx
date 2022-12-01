1bcac89c20feb61cc45d59a352fcacc6a0993203
Sahith Bodla <sahith.bodla@gmail.com>
2022-12-02 00:04:06 +0530

    feat(addcell): add addcell component

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTypedSelector } from '../hooks';
 import CellListItem from './cell-list-item';
+import AddCell from './add-cell';
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
@@ -8,9 +9,17 @@ const CellList: React.FC = () => {
   );
 
   const renderedCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <React.Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem cell={cell} />
+    </React.Fragment>
   ));
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      {renderedCells}
+      <AddCell nextCellId={null} />
+    </div>
+  );
 };
 
 export default CellList;
