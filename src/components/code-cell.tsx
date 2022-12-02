24ba3b57f8095b98a54c5eef581d5fdfc0e5e1b5
Sahith Bodla <sahith.bodla@gmail.com>
2022-12-03 00:25:53 +0530

    feat(show): extends JSX

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -18,11 +18,18 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const orderedCells = order.map((id) => data[id]);
     const cumulativeCode = [
       `
+        import _React from 'react';
+        import _ReactDOM from 'react-dom';
         const show = (value) => {
+          const root = document.querySelector('#root');
           if (typeof value === 'object') {
-            document.querySelector('#root').innerHTML = JSON.stringify(value);
+            if (value.$$typeof && value.props) {
+              _ReactDOM.render(value, root);
+            } else {
+              root.innerHTML = JSON.stringify(value);
+            }
           } else {
-            document.querySelector('#root').innerHTML = value;
+            root.innerHTML = value;
           }
         };
       `,
