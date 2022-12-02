ad3e349f7a706c0345e1d78de3421de7e940f8fa
Sahith Bodla <sahith.bodla@gmail.com>
2022-12-03 00:37:33 +0530

    fix(bug): showing values in all the following previews is fixed

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -16,11 +16,11 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const cumulativeCode = useTypedSelector((state) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
-    const cumulativeCode = [
-      `
+
+    const showFunc = `
         import _React from 'react';
         import _ReactDOM from 'react-dom';
-        const show = (value) => {
+        var show = (value) => {
           const root = document.querySelector('#root');
           if (typeof value === 'object') {
             if (value.$$typeof && value.props) {
@@ -32,10 +32,16 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             root.innerHTML = value;
           }
         };
-      `,
-    ];
+      `;
+    const showFuncNoop = 'var show = () => {}';
+    const cumulativeCode = [];
     for (let c of orderedCells) {
       if (c.type === 'code') {
+        if (c.id === cell.id) {
+          cumulativeCode.push(showFunc);
+        } else {
+          cumulativeCode.push(showFuncNoop);
+        }
         cumulativeCode.push(c.content);
       }
       if (c.id === cell.id) {
