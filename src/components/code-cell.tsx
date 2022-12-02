8904e7e11b82ab95f2a702526cdb778fd4ab148a
Sahith Bodla <sahith.bodla@gmail.com>
2022-12-03 00:03:46 +0530

    feat(show): extending functionality to objects

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -19,8 +19,12 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const cumulativeCode = [
       `
         const show = (value) => {
-          document.querySelector('#root').innerHTML = value
-        }
+          if (typeof value === 'object') {
+            document.querySelector('#root').innerHTML = JSON.stringify(value);
+          } else {
+            document.querySelector('#root').innerHTML = value;
+          }
+        };
       `,
     ];
     for (let c of orderedCells) {
